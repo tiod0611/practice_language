67625c36acdc079f67b03f2b5b772af1c69419f1
kyeul611 <gyul611@likelion.org>
2021-03-12 20:26:59 +0900

    add java file

---
+++ java/baekjoon/10171/src/고양이/cat.java
@@ -0,0 +1,14 @@
+package °í¾çÀÌ;
+
+public class cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+
+	}
+
+}
