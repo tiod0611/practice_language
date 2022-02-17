83f9c984504d635f76ff3a40cc6b3e12561c4324
kyeul611 <gyul611@likelion.org>
2022-02-17 19:54:12 +0900

    2022.02.17

--- python/백준/1018.py
+++ python/백준/1018.py
@@ -8,6 +8,8 @@ https://www.acmicpc.net/problem/1018
 출력
 첫째 줄에 지민이가 다시 칠해야 하는 정사각형 개수의 최솟값을 출력한다.
 '''
+import numpy as np
+
 array2D = [] # 체스판을 입력 받을 배열
 mins = []
 
@@ -19,23 +21,36 @@ for _ in range(N):
     row = [1 if x == 'B' else -1 for x in row] # B => 1, W => -1
     array2D.append(row)
 
+array2D = np.array(array2D)
 for r in range(N-7): # 아래로 8개씩, 1칸씩 이동
-    for w in range(M-7): # 오른쪽으로 8개씩, 1칸씩 이동
+    for c in range(M-7): # 오른쪽으로 8개씩, 1칸씩 이동
         minimum = 0
-        chess = array2D[r:r+8][w:w+8]
+        chess = array2D[r:r+8, c:c+8] # => 이거 안됨
         
-        for chessRow in chess:
-            for i in range(0,8,2):
-                if 0 != sum(chessRow[i:i+2]):
-                    minimum += 1
-                    print(minimum)
-               
-        mins.append(minimum)
+        # 짝수 줄의 원소를 한칸씩 이동. 맨뒤는 맨앞으로 자리한다
+        print("----")
+        print(chess)
+        for i in range(0,8,2):
+            temp = chess[i][-1]
+            chess[i][1:] = chess[i][:-1]
+            chess[i][0] = temp
+        print(chess)
+        arrayChess = np.array(chess)
+        colSum = np.einsum('ij->j', arrayChess)
+        print(colSum)
+        mins.append(np.sum(abs(colSum)))
 
-print(min(mins))
+print(mins)
+print(min(mins)//2)
 
 '''
 미완
-test case 4번에서 카운팅이 잘 못되는 문제가 있음.
-수정 예정
++ try1 (02.16)
+    test case 4번에서 카운팅이 잘 못되는 문제가 있음.
+    
++ try2 (02.17)
+    채스 배열이 모두 올바른지 검사하기 위해 대각선의 원소가 모두 같을 때만 올바른 체스판이고 다른 원소 하나당 수정해야할 원소라고 판단
+    코드 미완성
+    numpy 모듈 사용했으니 백준에선 허용하지 않음
+
 '''
