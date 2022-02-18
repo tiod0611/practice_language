67664a1987af47bb253e978403f487990f67294e
kyeul611 <gyul611@likelion.org>
2022-02-18 17:32:06 +0900

    2022.02.18

--- python/백준/1018.py
+++ python/백준/1018.py
@@ -8,8 +8,6 @@ https://www.acmicpc.net/problem/1018
 출력
 첫째 줄에 지민이가 다시 칠해야 하는 정사각형 개수의 최솟값을 출력한다.
 '''
-import numpy as np
-
 array2D = [] # 체스판을 입력 받을 배열
 mins = []
 
@@ -17,31 +15,33 @@ N, M = map(int, input().split(' '))
 
 for _ in range(N):
     row = input()
-    rowlist = [i for i in row]
-    row = [1 if x == 'B' else -1 for x in row] # B => 1, W => -1
+    # rowlist = [i for i in row]
+    # row = [1 if x == 'B' else -1 for x in row] # B => 1, W => -1
     array2D.append(row)
-
-array2D = np.array(array2D)
+print(array2D)
 for r in range(N-7): # 아래로 8개씩, 1칸씩 이동
     for c in range(M-7): # 오른쪽으로 8개씩, 1칸씩 이동
         minimum = 0
-        chess = array2D[r:r+8, c:c+8] # => 이거 안됨
-        
-        # 짝수 줄의 원소를 한칸씩 이동. 맨뒤는 맨앞으로 자리한다
-        print("----")
-        print(chess)
-        for i in range(0,8,2):
-            temp = chess[i][-1]
-            chess[i][1:] = chess[i][:-1]
-            chess[i][0] = temp
-        print(chess)
-        arrayChess = np.array(chess)
-        colSum = np.einsum('ij->j', arrayChess)
-        print(colSum)
-        mins.append(np.sum(abs(colSum)))
+        array1D = []
+        chess = [array2D[r+i][c:c+8] for i in range(8)] # 2차원 배열을 1차원으로 만들기
+        print("Chess", chess)
+        # array1D = [x for x in row for row in chess]    # 1차원 배열로 만들고
+        for row in chess:
+            for x in row:
+                array1D.append(x)
+        print("Arr1D", array1D)
+        oddArr = [array1D[odd] for odd in range(0, 64, 2)] # 홀수 인덱스만 가져오기
+        eveArr = [array1D[even] for even in range(1, 64, 2)] # 짝수 인덱스만 가져오기
 
+        print(oddArr)
+        print(eveArr)
+        for i in range(32):
+            if oddArr[i]==eveArr[i]:
+                minimum += 1
+
+        mins.append(minimum)
 print(mins)
-print(min(mins)//2)
+print(min(mins))
 
 '''
 미완
@@ -53,4 +53,8 @@ print(min(mins)//2)
     코드 미완성
     numpy 모듈 사용했으니 백준에선 허용하지 않음
 
++ try3 (02.18)
+    체스 배열을 1차원으로 만들고 짝수와 홀수 인덱싱으로 두 배열로 쪼갬
+    짝수 배열과 홀수 배열을 비교하여 서로 다른 원소 개수만큼 수정하면 된다고 봤음
+    => 예제 case 7에서 오답 출력
 '''
