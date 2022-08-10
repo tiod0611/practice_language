d500fc08d23892082f1f0436e734754f850f7ac6
Park Kyeul <gyul611@likelion.org>
2022-08-10 23:56:44 +0900

    2022.08.10

--- python/crawling/YoongiKim_AutoCrawler_copy/collect_links.py
+++ python/crawling/YoongiKim_AutoCrawler_copy/collect_links.py
@@ -1,2 +1,71 @@
+from telnetlib import EC
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotVisibleException, StaleElementReferenceException
+import platform
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions ad EC
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import os.path as osp
+
 class CollectLinks:
-    pass
+    def __init__(self, no_gui=False, proxy=None):
+        executable = ''
+
+        if platform.system() == 'Windows':
+            print('Detected OS : Windows')
+            executable = './chromedriver/chromedriver_win.exe'
+        elif platform.system() == 'Linux':
+            print('Detected OS: Linux')
+            executable = './chromedriver/chromedriver_linux'
+        elif platform.system() == 'Darwin':
+            print('Detected OS: Mac')
+            executable = './chromedriver/chromedriver_mac'
+        else:
+            raise OSError('Unknown OS Type')
+        
+        if not osp.exists(executable):
+            raise FileNotFoundError('Chromedriver file should be placed at {}'.format(executable))
+        
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+        if no_gui:
+            chrome_options.add_argument('--headless')
+        if proxy:
+            chrome_options.add_argument('--proxy-server={}'.format(proxy))
+        
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+        browser_version = 'Failed to detect version'
+        chromedriver_version = 'Failed to detect version'
+        major_version_different = False
+
+        if 'browserVersion' in self.browser.capabilities:
+            browser_version = str(self.browser.capabilities['browserVersion'])
+
+        if 'chrome' in self.browser.capabilities:
+            if 'chromedriverVersion' in self.browser.capabilities['chrome']:
+                chromedriver_version = str(self.browser.capabilities['chrome']['chromedriverVersion']).split(' ')[0]
+        
+        if browser_version.split('.')[0] != chromedriver_version.split('.')[0]:
+            major_version_different = True
+        
+        print('_________________________')
+        print('Current web-browser version:\t{}'.format(browser_version))
+        print('Current chrome-driver version:\t{}'.format(chromedriver_version))
+        if major_version_different:
+            print('warning: Version different')
+            print('Download correct version at "http://chromedriver.chromium.org/downloads" and place in "./chromedriver"')
+            print('_________________________')
+
+    
+    def get_scroll(self):
+        pos = self.browser.execute_script("return window.pageYOffset;")
+        return pos
+    
+    def wait_and_clict(self, xpath):
+        pass
