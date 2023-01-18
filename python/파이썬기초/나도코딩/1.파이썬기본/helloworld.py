c7e456bd8b1efe50c245d8ab675972a0422e34dd
kyeul611 <gyul611@likelion.org>
2023-01-18 23:39:03 +0900

    2023.01.18: python practice Exception

--- python/파이썬기초/나도코딩/1.파이썬기본/helloworld.py
+++ python/파이썬기초/나도코딩/1.파이썬기본/helloworld.py
@@ -265,46 +265,127 @@
 
 # game_over()
 
-# 퀴즈 : 부동산 프로그램 작성하기
+# # 퀴즈 : 부동산 프로그램 작성하기
 
-# 출력 예시
-# 총 3대의 매물이 있습니다.
-# 강남 아파트 매매 10억 2010년
-# 마포 오피스텔 전세 5억 2007년
-# 송파 빌라 월세 500/50 2000년
+# # 출력 예시
+# # 총 3대의 매물이 있습니다.
+# # 강남 아파트 매매 10억 2010년
+# # 마포 오피스텔 전세 5억 2007년
+# # 송파 빌라 월세 500/50 2000년
 
-class House:
-    # 매물 정보 초기화
-    count = 0
+# class House:
+#     # 매물 정보 초기화
+#     count = 0
 
-    def __init__(self, location, house_type, deal_type, price, completion_year):
+#     def __init__(self, location, house_type, deal_type, price, completion_year):
         
-        # 매물 개수
-        House.count += 1
-        # 매물 정보
-        self.location = location
-        self.house_type = house_type
-        self.deal_type = deal_type
-        self.price = price
-        self.completion_year = completion_year
+#         # 매물 개수
+#         House.count += 1
+#         # 매물 정보
+#         self.location = location
+#         self.house_type = house_type
+#         self.deal_type = deal_type
+#         self.price = price
+#         self.completion_year = completion_year
 
-    def show_detail(self):
-        print("{} {} {} {} {}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
+#     def show_detail(self):
+#         print("{} {} {} {} {}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
 
-    def show_count():
-        print("총 {}대의 매물이 있습니다.".format(House.count))
+#     def show_count():
+#         print("총 {}대의 매물이 있습니다.".format(House.count))
 
 
-# 객체 생성
-property1 = House("강남", "아파트", '매매','10억', '2010년')
-property2 = House('마포', '오피스텔', '전세', '5억', '2007년')
-property3 = House('송파', '빌라', '월세', '500/50', '2000년')
+# # 객체 생성
+# property1 = House("강남", "아파트", '매매','10억', '2010년')
+# property2 = House('마포', '오피스텔', '전세', '5억', '2007년')
+# property3 = House('송파', '빌라', '월세', '500/50', '2000년')
 
-houses = []
-houses.append(property1)
-houses.append(property2)
-houses.append(property3)
+# houses = []
+# houses.append(property1)
+# houses.append(property2)
+# houses.append(property3)
 
-House.show_count()
-for house in houses:
-    house.show_detail()
+# House.show_count()
+# for house in houses:
+#     house.show_detail()
+
+# ============================예외처리==============================
+
+# 의도적으로 에러 발생시키기
+
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+    
+#     def __str__(self):
+#         return self.msg
+
+# try: 
+#     print("한 자리 숫자 전용 계산기")
+#     num1 = int(input("첫번째 숫자"))
+#     num2 = int(input("두번째 숫자"))
+
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값: {}, {}".format(num1, num2))
+
+#     print(num1, num2)
+
+# except ValueError:
+#     print("한자리만 넣으세요")
+
+# except BigNumberError as e:
+#     print("제발~! 한자리만 넣으세요")
+#     print(e)
+
+# # 코드의 정상/비정상과 관계없이 반드시 실행되는 부분
+# finally:
+#     print('여기는 반드시 실행됩니다.')
+
+# 퀴즈 치킨 자동 주문 시스템
+
+# 조건 1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError  
+#         출력 메시지: "잘못된 값을 입력하였습니다."
+
+# 조건 2 : 치킨량은 10마리, 치킨 소진시 사용자 정의 에러[SoldOutError]를 발생 후 종료
+#         출력 메시지: "w재고가 소진되어 더 이상 주문을 받지 않습니다."
+
+# 코드
+
+class SoldOutError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+
+
+chicken = 10
+waiting = 1 # 홀 안엔느 현재 만석. 대기번호 1부터 시작
+
+while(True):
+    try:
+        print('[남은 치킨 : {}]'.format(chicken))
+        
+        order = int(input("치킨 몇 마리 주문하시겠습니까? "))
+        print(type(order))
+        if str(type(order)) != "<class 'int'>" or order < 1:
+            raise ValueError
+
+        if order > chicken: #남은 치킨보다 주문량이 많을 때
+            print("재료가 부족합니다")
+        
+        else:
+            print("[대기번호 {}] {} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
+            
+        if chicken <= 0:
+            raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+
+    except ValueError:
+        print("잘못된 값을 입력하였습니다. ")
+        continue
+
+    except SoldOutError as e:
+        print(e)
+        break
