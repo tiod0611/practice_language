c9363d5293ba072ed840131e92607313b1b3b3ed
kyeul611 <gyul611@likelion.org>
2023-01-20 17:54:22 +0900

    2023.01.20: practice crawling

--- python/파이썬기초/나도코딩/3.웹스크래핑/4_re.py
+++ python/파이썬기초/나도코딩/3.웹스크래핑/4_re.py
@@ -26,6 +26,19 @@ else:
 #  match용 함수
 def print_match(m):
     if m:
-        print(m.group())
+        print("group", m.group()) # 일치하는 문자열 출력
+        print("string", m.string) # 입력받은 문자열
+        print("start", m.start()) # 일치하는 문자열의 index
+        print("end", m.end()) # 마지막 index
+        print("span", m.span()) # 문자열의 시작과 끝의 index
+
     else:
         print("매칭되지 않음")
+
+m = p.search("good care") # 주어진 문자열 중에 일치하는 게 있는 지 확인
+print_match(m)
+
+# find_all
+
+lst = p.findall("good care cafe") # 일치하는 모든 것을 리스트 형태로 반환
+print(lst) 
