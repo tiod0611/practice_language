4b3aea411cbaba8278d55068aaee2f279655a3e7
kyeul611 <gyul611@likelion.org>
2023-06-12 19:44:54 +0900

    edit content

--- docker/docker-compose/python-with-mysql/DBController.py
+++ docker/docker-compose/python-with-mysql/DBController.py
@@ -4,7 +4,7 @@ import pymysql
 class DBUpdater:
     def __init__(self, db_pw):
         conn = pymysql.connect(
-            host='localhost',
+            host='172.0.0.1',
             port=3306,
             user='root',
             password=db_pw
