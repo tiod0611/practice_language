545d0aba0b093ef12ed9ed950e76332da715a31a
kyeul611 <gyul611@likelion.org>
2023-10-27 18:37:24 +0900

    writed class Trainer()

--- python/딥러닝/패캠_김기현/LLM_with_HF/simple_ntc/trainer.py
+++ python/딥러닝/패캠_김기현/LLM_with_HF/simple_ntc/trainer.py
@@ -165,4 +165,45 @@ class MyEngine(Engine):
 
 
 class Trainer():
-    pass
+    
+    def __init__(self, config):
+        self.config = config
+
+    def train(self,model, crit, optimizer, train_loader, valid_loader):
+
+        train_engine = MyEngine(
+            MyEngine.train,
+            model, crit, optimizer, self.config
+        )
+        validation_engine = MyEngine(
+            MyEngine.validate,
+            model, crit, optimizer, self.config
+        )
+
+        MyEngine.attach(
+            train_engine,
+            validation_engine,
+            verbose=self.config.verbose
+        )
+
+        def run_validation(engine, validation_engine, valid_loader):
+            validation_engine.run(valid_loader, max_epochs=1)
+
+        train_engine.add_event_handler(
+            Events.EPOCH_COMPLETED, # event
+            run_validation, # fucntion
+            validation_engine, valid_loader, # arguments
+        )
+        validation_engine.add_event_handler(
+            Events.EPOCH_COMPLETED, # event
+            MyEngine.check_best, # function
+        )
+
+        train_engine.run(
+            train_loader,
+            max_epochs=self.config.n_epochs,
+        )
+
+        model.load_state_dict(validation_engine.best_model)
+
+        return model
