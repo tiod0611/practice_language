6c47f1a5c277da6a4e3ec178150eed91839839ec
Park Kyeul <gyul611@likelion.org>
2023-10-29 16:12:39 +0900

    writing scripts

---
+++ python/딥러닝/패캠_김기현/LLM_with_HF/bert_dataset.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import Dataset
+
+class TextClassificationDataset(Dataset):
+    def __init__(self, texts, labels):
+        self.texts = texts
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, item):
+        text = str(self.texts[item])
+        label = self.labels[item]
+
+        return text, label
